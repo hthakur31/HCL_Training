e7744441865659b2b61a36b5f8b5d779d8e20775
ReluConsultancy Submission <182757478+hthakur31@users.noreply.github.com>
2026-01-09 16:23:58 +0530

    Authentication popup update

--- SeleniumPractice/jan9_26/authenticationPopUp.java
+++ SeleniumPractice/jan9_26/authenticationPopUp.java
@@ -10,10 +10,10 @@ public class authenticationPopUp {
 		WebDriver driver = new ChromeDriver();
 		String user = "admin";
 		String password = "admin";
-		String URL = "https://" + user + ":" + password + "@" + "the-internet.herokuapp.com/basic_auth";
-		//driver.get(URL);
+		String URL = "https://" + user + ":" + password + "@" + "the-internet.herokuapp.com/basic_auth";// with username and password
+		driver.get(URL);
 		
-		driver.get("https://the-internet.herokuapp.com/basic_auth");
+		//driver.get("https://the-internet.herokuapp.com/basic_auth"); // without username and password
 		System.out.println("Page title : "+driver.getTitle());
 		Thread.sleep(3000);
 		driver.quit();
