bd9dfe94c0f71930d36c9916ee55c56ca87ff1ce
ReluConsultancy Submission <182757478+hthakur31@users.noreply.github.com>
2026-01-13 11:23:21 +0530

    modified

--- SeleniumPractice/jan12_26/iframeHandling.java
+++ SeleniumPractice/jan12_26/iframeHandling.java
@@ -1,7 +1,4 @@
 package jan12_26;
-
-import java.util.ArrayList;
-import java.util.List;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -22,7 +19,7 @@ public class iframeHandling {
 		
 		//using WebElement
 		WebElement fe = driver.findElement(By.id("frame2"));
-		driver.switchTo().frame("fe");
+		driver.switchTo().frame(fe);
 		
 		//switch back to previous frame
 		driver.switchTo().defaultContent();
