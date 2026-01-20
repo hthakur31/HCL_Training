484214aa683dd237e8fdc05c722b318c225cc599
ReluConsultancy Submission <182757478+hthakur31@users.noreply.github.com>
2026-01-20 11:56:56 +0530

    random errors fix

--- SeleniumPractice/csvtest/CSVTest.java
+++ SeleniumPractice/csvtest/CSVTest.java
@@ -9,27 +9,27 @@ public class CSVTest {
 	public static void main(String[] args) throws IOException {
 		
 		WebDriver driver = new ChromeDriver();
-		String csvFile = "F:\\eclipse-java-2025-09-R-win32-x86_64\\HCL_Training\\SeleniumPractice\\csvtest\\Test.csv";
+		String csvFile = "F:\\eclipse-java-2025-09-R-win32-x86_64\\HCL_Training\\SeleniumPractice\\csvtest\\data.csv";
 		BufferedReader br = new BufferedReader(new FileReader(csvFile));
 		String line;
-		boolean FirstLine = true;
-		while ((line =br.readLine()) != null) {
+		//boolean FirstLine = true;
+		while ((line = br.readLine()) != null) {
 			
-			if(FirstLine) {
-				
-				FirstLine = false;
-				continue;
-			}
-			
+//			if(FirstLine) {
+//				
+//				FirstLine = false;
+//				continue;
+//			}
+//	
 			String[] data = line.split(",");
 			String username = data[0];	
 			String password = data[1];
-			driver.get("");
-			driver.findElement(By.xpath("")).clear();
-			driver.findElement(By.xpath("")).clear();
-			driver.findElement(By.xpath("")).sendKeys();
-			driver.findElement(By.xpath("")).sendKeys();
-			driver.findElement(By.xpath("")).click();
+			driver.get("https://onlinefeestechnocrats.in/LoginStudent.aspx?type=F");
+			driver.findElement(By.xpath("//input[@id='Login1_UserName']")).clear();
+			driver.findElement(By.xpath("//input[@id='Login1_Password']")).clear();
+			driver.findElement(By.xpath("//input[@id='Login1_UserName']")).sendKeys(username);
+			driver.findElement(By.xpath("//input[@id='Login1_Password']")).sendKeys(password);
+			driver.findElement(By.xpath("//input[@id='Login1_LoginButton']")).click();
 			
 			String un = "aaaa";
 			if(un.equals(username)) {
@@ -40,8 +40,6 @@ public class CSVTest {
 				System.out.println("login unsuccessful");
 			}
 		}
-		br.close();
-		driver.close();
 	}
 
 }
