3a8b39c5f556ec146fa62df32aa4ad43ff8fc80a
ReluConsultancy Submission <182757478+hthakur31@users.noreply.github.com>
2026-01-21 13:25:01 +0530

    registrationForm using POM

---
+++ SeleniumPractice/jan21_26/registrationForm.java
@@ -0,0 +1,25 @@
+package jan21_26;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class registrationForm {
+	WebDriver driver;
+	public registrationForm(WebDriver driver){
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	@FindBy(id = "un") WebElement username;
+	@FindBy(id = "ps") WebElement password;
+	@FindBy(id = "male") WebElement radio;
+	@FindBy(id = "btn") WebElement btnSubmit;
+	
+	public void testRegistration(String user, String pass ) {
+		username.sendKeys(user);
+		password.sendKeys(pass);
+		radio.click();
+		btnSubmit.click();
+	}
+}
