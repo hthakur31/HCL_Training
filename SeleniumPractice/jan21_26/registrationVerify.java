56b9d27120487c6f04fd81e082652123eebc8678
ReluConsultancy Submission <182757478+hthakur31@users.noreply.github.com>
2026-01-21 14:09:31 +0530

    fixed some alignment and close browser

--- SeleniumPractice/jan21_26/registrationVerify.java
+++ SeleniumPractice/jan21_26/registrationVerify.java
@@ -5,11 +5,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class registrationVerify {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
-		driver.get("");
+		driver.get("F:\\eclipse-java-2025-09-R-win32-x86_64\\HCL_Training\\SeleniumPractice\\jan21_26\\registrationform.html");
 		registrationForm rf = new registrationForm(driver);
 		rf.testRegistration("harsh", "harsh123");
+		Thread.sleep(3000);
+		driver.close();
 	}
 
 }
