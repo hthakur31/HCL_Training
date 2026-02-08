12c8f494476a284b7eba45be2114496506d860fb
ReluConsultancy Submission <182757478+hthakur31@users.noreply.github.com>
2026-02-08 21:52:38 +0530

    Modified

--- SeleniumPractice/jan21_26/registrationForm.java
+++ SeleniumPractice/jan21_26/registrationForm.java
@@ -1,5 +1,7 @@
  package jan21_26;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -16,10 +18,19 @@ public class registrationForm {
 	@FindBy(id = "male") WebElement radio;
 	@FindBy(id = "btn") WebElement btnSubmit;
 	
+	JavascriptExecutor js; 
 	public void testRegistration(String user, String pass ) {
-		username.sendKeys(user);
-		password.sendKeys(pass);
-		radio.click();
-		btnSubmit.click();
+//		username.sendKeys(user);
+//		password.sendKeys(pass);
+//		radio.click();
+//		btnSubmit.click();
+		
+	}
+	public void ByJs() {
+		js= (JavascriptExecutor)driver;
+		js.executeScript("arguments[0].setAttribute('value','user')",username);
+		js.executeScript("arguments[0].setAttribute('value','pass')",password);
+		js.executeScript("arguments[0].click()",radio);
+		js.executeScript("arguments[0].click()",btnSubmit);
 	}
 }
--- SeleniumPractice/jan21_26/registrationVerify.java
+++ SeleniumPractice/jan21_26/registrationVerify.java
@@ -7,11 +7,11 @@ public class registrationVerify {
 
 	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
-		driver.get("F:\\eclipse-java-2025-09-R-win32-x86_64\\HCL_Training\\SeleniumPractice\\jan21_26\\registrationform.html");
 		registrationForm rf = new registrationForm(driver);
-		rf.testRegistration("harsh", "harsh123");
+		driver.get("F:\\eclipse-java-2025-09-R-win32-x86_64\\HCL_Training\\SeleniumPractice\\jan21_26\\registrationform.html");
+		//rf.testRegistration("harsh", "harsh123");
+		rf.ByJs();
 		Thread.sleep(3000);
-		driver.close();
+		driver.quit();
 	}
-
 }
